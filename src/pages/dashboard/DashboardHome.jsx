1b0d6722c3dfa37b57f8e927089061360ef5ac61
ZamZamGraphics <zamzamprintmedia@gmail.com>
2024-05-10 19:19:50 +0600

    Working in Dashboad Card Item

--- src/pages/dashboard/DashboardHome.jsx
+++ src/pages/dashboard/DashboardHome.jsx
@@ -1,9 +1,24 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
+import StudentIcon from "../../assets/images/graduation-hat.svg";
+import DashboardCard from "./DashboardCard";
 
 function DashboardHome() {
   return (
     <Box>
-      <Typography variant="h5">Dashboard Home Page</Typography>
+      <Typography variant="h5" sx={{ marginBottom: 3 }}>
+        Welcome, Admin
+      </Typography>
+      <Grid container spacing={2} autoComplete="off">
+        <Grid item xs={2}>
+          <DashboardCard 
+          icon={StudentIcon} 
+          iconBgColor="#EBF3FF"
+          title="Total Students"
+          value="1570"
+          color="#1F69D8"
+          />
+        </Grid>
+      </Grid>
     </Box>
   );
 }
